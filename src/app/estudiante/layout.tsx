b2cb070070d140eca833be3cfc774adae4832642
Jesús Sánchez <jesus04122@gmail.com>
2026-02-21 21:30:20 +0100

    fix: components width overflow

--- src/app/estudiante/layout.tsx
+++ src/app/estudiante/layout.tsx
@@ -7,9 +7,9 @@ export default function StudentLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen bg-white">
+    <div className="flex min-h-screen bg-white overflow-hidden">
       <SideBarStudent />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <DashboardHeader />
         <main className="p-8">{children}</main>
       </div>
